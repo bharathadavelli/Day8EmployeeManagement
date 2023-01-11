a931a07277b1c851ea75f66b7ce723e0c48affdd
“bharathadavelli” <bharathadavelli@gmail.com>
2023-01-11 16:17:16 +0530

    [Bharath]-Max100hrs or 20 workingdays

--- Day8EmployeeManagement/Day8EmployeeManagement/Program.cs
+++ Day8EmployeeManagement/Day8EmployeeManagement/Program.cs
@@ -9,7 +9,9 @@
             //UC2DailyWage.Empdailywage();
             //UC3Parttime.parttime();
             //UC4Usingswitchcase.switchcase();
-            UC5Empwagefor20workingdays.empwage();
+            //UC5Empwagefor20workingdays.empwage();
+            UC6100hrsor20days.Empwage100hrsor20days();
+
         }
     }
 }
