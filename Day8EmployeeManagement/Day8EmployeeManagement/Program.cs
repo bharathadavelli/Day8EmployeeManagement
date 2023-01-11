2d13cbb34a3f16733296d5bdb556a30fe4c13c4c
“bharathadavelli” <bharathadavelli@gmail.com>
2023-01-11 14:39:15 +0530

    [Bharath]-Welcome msg

---
+++ Day8EmployeeManagement/Day8EmployeeManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day8EmployeeManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day8 EmployeeManagement");
+        }
+    }
+}
